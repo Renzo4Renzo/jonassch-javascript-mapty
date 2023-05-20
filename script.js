1a074fd47524fa0f517c416bfb0d428a2b3d8f83
Renzo Diaz <renzodiaz554@gmail.com>
2023-05-20 13:37:22 -0500

    Displaying a Map Marker

--- script.js
+++ script.js
@@ -15,27 +15,37 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coordinates = [latitude, longitude];
 
       const map = L.map('map').setView(coordinates, 14);
+      //   console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coordinates)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat: clickedLatitude, lng: clickedLongitude } = mapEvent.latlng;
+        L.marker([clickedLatitude, clickedLongitude])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent(`Workout`)
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your position!`);
     }
   );
 }
-
-// console.log(myName);
